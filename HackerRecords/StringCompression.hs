41e64b139fb032469e9f845a1e5f360dc48a8592
Samuel Escalera <samuel.escalera.9413603@usalesiana.edu.bo>
2024-04-23 22:27:51 -0400

    Commit

--- HackerRecords/StringCompression.hs
+++ HackerRecords/StringCompression.hs
@@ -6,3 +6,9 @@ compressChar xs = head xs : show (length xs)
 
 compressString :: String -> String
 compressString = concatMap compressChar.group
+
+
+compressStringFunctor :: String -> String
+compressStringFunctor = concatMap (compressChar <$> id) . group
+
+--- "aaabbbcccccdd"
--- Slide/Main.hs
+++ Slide/Main.hs
@@ -4,9 +4,15 @@ import Scanner(scanner)
 import UU.Parsing
 import Parser
 
+import System.IO
+
 main :: IO ()
 main = do input <- readFile "slide.p5"
           let token = scanner input
           putStrLn(show token) 
           tree <- parseIO pSlides token
           putStrLn (show tree)
+
+          let contenido = "<!DOCTYPE html>\n<html>\n<head>\n<title>¡Hola, Mundo!</title>\n</head>\n<body>\n<h1>¡Hola, Mundo!</h1>\n<p>Este es un archivo HTML creado desde Haskell.</p>\n</body>\n</html>"
+    writeFile "index.html" contenido
+    putStrLn "Archivo index.html creado correctamente."
